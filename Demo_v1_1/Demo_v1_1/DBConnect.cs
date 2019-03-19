c675a14b883968c50b348ff57e99dcd393e15e83
Özgür Hasan Aytar <zrhaytar@gmail.com>
2019-03-20 01:58:46 +0300

    rfid testing

---
+++ Demo_v1_1/Demo_v1_1/DBConnect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Demo_v1_1
+{
+    class DBConnect
+    {
+        public bool DBCon() {
+            try
+            {
+                string connetionString = null;
+                SqlConnection cnn;
+                connetionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=MedicineCMS;Integrated Security=True";
+                cnn = new SqlConnection(connetionString);
+                cnn.Open();
+                //cnn.Close();
+                return true;
+            }
+            catch(Exception) {
+                    return false;
+                    }
+            }
+    }
+}
---
+++ Demo_v1_1/Demo_v1_1/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Demo_v1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textRFID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // textRFID
+            // 
+            this.textRFID.Location = new System.Drawing.Point(74, 28);
+            this.textRFID.Name = "textRFID";
+            this.textRFID.Size = new System.Drawing.Size(146, 22);
+            this.textRFID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "RFID";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM7";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 213);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textRFID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textRFID;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
